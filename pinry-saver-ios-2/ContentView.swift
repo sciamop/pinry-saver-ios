740892d2ec55aad12b5ab0bbc85d1462cc48406b
Todd Faulls <toddfaulls@gmail.com>
2025-10-03 11:03:01 -0400

    Initial Commit

---
+++ pinry-saver-ios-2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pinry-saver-ios-2
+//
+//  Created by Todd Faulls on 10/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ pinry-saver-ios-2/pinry_saver_ios_2App.swift
@@ -0,0 +1,17 @@
+//
+//  pinry_saver_ios_2App.swift
+//  pinry-saver-ios-2
+//
+//  Created by Todd Faulls on 10/3/25.
+//
+
+import SwiftUI
+
+@main
+struct pinry_saver_ios_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
